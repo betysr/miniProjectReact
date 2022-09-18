14d2f518b52159119befa418846030b5ab400e25
betysr <betysr96@gmail.com>
2022-09-18 14:16:10 +0200

    weather app done

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,33 +1,12 @@
 import React from 'react';
+import WeatherApi from './components/WeatherApi';
 
 function App() {
+
   return (
-    <div className="App">
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>Dallas</p>
-          </div>
-          <div className="temp">
-            <h1>65°F</h1>
-          </div>
-          <div className="description">
-            <p>Clouds</p>
-          </div>
-        </div>
-        <div className="bottom">
-          <div className="feels">
-            <p>60°F</p>
-          </div>
-          <div className="humidity">
-              <p>20%</p>
-          </div>
-          <div className="wind">
-            12 MPH
-          </div>
-        </div>
-      </div>
-    </div>
+    <div className="app">
+      <WeatherApi></WeatherApi>
+     </div>
   );
 }
 
